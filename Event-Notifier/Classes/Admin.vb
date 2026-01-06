e2cf49cf6305842b45dca91cca7c5f49f90ec5e3
Shawn Kim <shawnsvkim@gmail.com>
2026-01-06 13:52:01 +0300

    Update Admin.vb

--- Event-Notifier/Classes/Admin.vb
+++ Event-Notifier/Classes/Admin.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Admin
-
+' Properties matching Table 2: Admins in the guide
+    Public Property AdminID As Integer
+    Public Property Username As String
+    Public Property Email As String
+    Public Property FullName As String
+    ' We WILL NOT store the password here for security purposes.
 End Class
