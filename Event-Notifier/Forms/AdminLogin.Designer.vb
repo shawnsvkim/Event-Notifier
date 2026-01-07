f4dbc84c6bc76168a03b95b713dc30f6f67bb244
Shawn Kim <shawnsvkim@gmail.com>
2026-01-07 11:59:35 +0300

    Update AdminLogin.Designer.vb
    added security masking, added error label, and silenced any resource error

--- Event-Notifier/Forms/AdminLogin.Designer.vb
+++ Event-Notifier/Forms/AdminLogin.Designer.vb
@@ -22,9 +22,147 @@ Partial Class AdminLogin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AdminLogin))
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.logo = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblError = New System.Windows.Forms.Label() ' Added Error Label
+        CType(Me.logo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnLogin
+        '
+        Me.btnLogin.BackColor = System.Drawing.Color.SteelBlue
+        Me.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLogin.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLogin.ForeColor = System.Drawing.Color.White
+        Me.btnLogin.Location = New System.Drawing.Point(339, 468)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(419, 55)
+        Me.btnLogin.TabIndex = 3
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("SimSun", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Gray
+        Me.Label2.Location = New System.Drawing.Point(175, 338)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 24)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Username"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("SimSun", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Gray
+        Me.Label3.Location = New System.Drawing.Point(175, 400)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(127, 24)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Password "
+        '
+        'txtUsername
+        '
+        Me.txtUsername.BackColor = System.Drawing.Color.Moccasin
+        Me.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtUsername.Location = New System.Drawing.Point(339, 338)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(419, 31)
+        Me.txtUsername.TabIndex = 1
+        '
+        'txtPassword
+        '
+        Me.txtPassword.BackColor = System.Drawing.Color.Moccasin
+        Me.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtPassword.Location = New System.Drawing.Point(339, 400)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42) ' SECURITY FIX: Added * mask
+        Me.txtPassword.Size = New System.Drawing.Size(419, 31)
+        Me.txtPassword.TabIndex = 2
+        '
+        'logo
+        '
+        ' Me.logo.Image = CType(resources.GetObject("logo.Image"), System.Drawing.Image) ' Commented out to prevent crash
+        Me.logo.Location = New System.Drawing.Point(377, 89)
+        Me.logo.Name = "logo"
+        Me.logo.Size = New System.Drawing.Size(343, 243)
+        Me.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.logo.TabIndex = 6
+        Me.logo.TabStop = False
+        Me.logo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle ' Added border so you can see where image goes
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(357, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(380, 64)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "ADMIN LOGIN"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Label4.Location = New System.Drawing.Point(460, 569)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(184, 25)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Authorized users only"
+        '
+        'lblError
+        '
+        Me.lblError.AutoSize = True
+        Me.lblError.ForeColor = System.Drawing.Color.Red
+        Me.lblError.Location = New System.Drawing.Point(339, 440)
+        Me.lblError.Name = "lblError"
+        Me.lblError.Size = New System.Drawing.Size(0, 25)
+        Me.lblError.TabIndex = 9
+        Me.lblError.Visible = False
+        '
+        'AdminLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "AdminLogin"
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(1026, 603)
+        Me.Controls.Add(Me.lblError)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.logo)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnLogin)
+        Me.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "AdminLogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        CType(Me.logo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+        
     End Sub
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents logo As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lblError As System.Windows.Forms.Label
 End Class
